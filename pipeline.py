a76c34290b4c1fa476f38f66be767b09d886238f
Treneti <zheni@ephemeronresearch.com>
2024-09-11 16:33:26 +0300

    dagster working and test notebook showing the results

--- pipeline.py
+++ pipeline.py
@@ -1,62 +1,114 @@
-import json
-
+import os
 import pandas as pd
-import requests
+import yfinance as yf
+from datetime import date
+from sklearn.ensemble import IsolationForest
+import pickle
+from scipy.stats import norm
+from dagster import asset, Definitions, define_asset_job, AssetSelection, ScheduleDefinition, DefaultScheduleStatus
+from scores_calculations import annualized_return, max_drawdown, annualized_volatility, annualized_sharpe_ratio, calmar_ratio, information_coefficient, hit_rate
 
-from dagster import Config, MaterializeResult, MetadataValue, asset, Definitions, define_asset_job, AssetSelection, ScheduleDefinition, DefaultScheduleStatus
+OUTDIR = '/app/data'
+TICKERS_LOAD_URL: str = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+START_DATE = '2017-01-01'
+
+def get_date_path(today_date=None):
+    if today_date is None:
+        today_date = str(date.today())
+    return f'{OUTDIR}/{today_date}'
+
+def set_today_directory(today_path):
+    if not os.path.exists( f'{today_path}'):
+        os.mkdir( f'{today_path}')
+        os.mkdir(f'{today_path}/raw_data_tickers')
+
+@asset(group_name='strategy_calc')
+def load_tickers():
+    # Giving bias with this small universe 
+    set_today_directory(get_date_path())
+    tables = pd.read_html(TICKERS_LOAD_URL)
+    sp500_table = tables[0]
+    tickers = sp500_table['Symbol'].tolist()
+    with open(f'{get_date_path()}/tickers_list.pkl', 'wb') as fp:
+        pickle.dump(tickers, fp)
+
+@asset(deps=[load_tickers],group_name='strategy_calc')
+def load_ticker_data():
+    tickers_list = []
+    with open (f'{get_date_path()}/tickers_list.pkl', 'rb') as fp:
+        tickers_list = pickle.load(fp)
+    #TODO: could be parallelized  
+    for ticker in tickers_list:
+        data = yf.download(ticker, start=START_DATE)
+        data['ticker'] = ticker
+        data.reset_index().to_parquet(f'{get_date_path()}/raw_data_tickers/{ticker}.parquet')
 
 
-class HNStoriesConfig(Config):
-    top_stories_limit: int = 10
-    hn_top_story_ids_path: str = "hackernews_top_story_ids.json"
-    hn_top_stories_path: str = "hackernews_top_stories.csv"
+@asset(deps=[load_ticker_data],group_name='strategy_calc')
+def combine_data():
+    tickers_list = []
+    with open (f'{get_date_path()}/tickers_list.pkl', 'rb') as fp:
+        tickers_list = pickle.load(fp)
+    dfs = []
+    for ticker in tickers_list:
+        df = pd.read_parquet(f'{get_date_path()}/raw_data_tickers/{ticker}.parquet')
+        dfs.append(df)
+    combined_data = pd.concat(dfs,axis=0).reset_index().rename(str.lower, axis='columns').set_index(['date','ticker']).sort_index().drop(columns=['index'])
+    combined_data.reset_index().to_parquet(f'{get_date_path()}/combined_data.parquet')
+
+@asset(deps=[combine_data],group_name='strategy_calc')
+def clean_data():
+    data = pd.read_parquet(f'{get_date_path()}/combined_data.parquet').set_index(['date','ticker'])
+    isolation_forest = IsolationForest(contamination=0.01, random_state=42)
+    predictions = isolation_forest.fit_predict(data)
+    cleaned_data = data[predictions == 1].copy()
+    cleaned_data['target_1d'] = cleaned_data['adj close'].groupby('ticker').pct_change()
+    cleaned_data.dropna().reset_index().to_parquet(f'{get_date_path()}/combined_cleaned_data.parquet')
+
+@asset(deps=[clean_data],group_name='strategy_calc')
+def calc_strategy():
+    # https://www.msci.com/documents/1296102/8473352/Volatility-brochure.pdf/f9cac8cb-f467-470d-9292-0298a597799e
+    data = pd.read_parquet(f'{get_date_path()}/combined_cleaned_data.parquet').set_index(['date','ticker'])
+    data['return_1d'] = data['target_1d'].groupby('ticker').shift(1)
+    data['volatilty_1y'] = data['return_1d'].groupby('ticker').ewm(halflife=126).std().droplevel(0)
+    data['vol_perc'] = data['volatilty_1y'].groupby('date').rank(pct=True).dropna()
+    data['alpha'] = data.groupby('date')['vol_perc'].transform(lambda x: norm.ppf(x.clip(lower=0.000001, upper=0.999999)))
+    data[['alpha','target_1d']].dropna().reset_index().to_parquet(f'{get_date_path()}/res_data.parquet')    
+
+@asset(deps=[calc_strategy],group_name='strategy_calc')
+def calc_scores():
+    data = pd.read_parquet(f'{get_date_path()}/res_data.parquet').set_index(['date','ticker'])
+    data['alpha_norm'] = data.groupby('date')['alpha'].transform(lambda x: x - x.mean())
+    data['alpha_norm'] = data['alpha_norm'].groupby('date').transform(lambda x: x * (2 / x.abs().sum()))
+    
+    res_df = pd.DataFrame(((data['alpha_norm'] * data['target_1d']).groupby('date').sum()),columns=['returns'])
+    res_df['pnl'] = (1 + res_df['returns'].cumsum())
+    res_df.dropna().reset_index().to_parquet(f'{get_date_path()}/res_rets.parquet')
+    scores = {}
+    scores['sharpe'] = annualized_sharpe_ratio(res_df['returns'])
+    scores['max_dd'] = max_drawdown(res_df['returns'])
+    scores['volatility'] = annualized_volatility(res_df['returns'])
+    scores['ann_returns'] = annualized_return(res_df['returns'])
+    scores['calmar_ratio'] = calmar_ratio(res_df['returns'])
+    scores['information_coefficient']  = information_coefficient(data['alpha_norm'],data['target_1d'])
+    scores['hit_rate']  = hit_rate(data['alpha_norm'],data['target_1d'])
+    scores_df = pd.DataFrame.from_dict(scores, orient='index',columns=['score'])
+    scores_df.dropna().reset_index().to_parquet(f'{get_date_path()}/scores.parquet')    
 
 
-@asset(group_name='data_loading')
-def data_fethcing(config: HNStoriesConfig):
-    pass
-
-
-@asset(deps=[data_fethcing],group_name='test_gr_name')
-def hackernews_top_stories(config: HNStoriesConfig) -> MaterializeResult:
-    """Get items based on story ids from the HackerNews items endpoint."""
-    with open(config.hn_top_story_ids_path, "r") as f:
-        hackernews_top_story_ids = json.load(f)
-
-    results = []
-    for item_id in hackernews_top_story_ids:
-        item = requests.get(
-            f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json"
-        ).json()
-        results.append(item)
-
-    df = pd.DataFrame(results)
-    df.to_csv(config.hn_top_stories_path)
-
-    print("working here afhnowainhfogwein")
-
-    return MaterializeResult(
-        metadata={
-            "num_records": len(df),
-            "preview": MetadataValue.md(str(df[["title", "by", "url"]].to_markdown())),
-        }
-    )
-
-
-ecommerce_job = define_asset_job(
-    "ecommerce_job", AssetSelection.groups("test_gr_name")
+strategy_calc_job = define_asset_job(
+    "strategy_calc_job", AssetSelection.groups("strategy_calc")
 )
 
-
-ecommerce_schedule = ScheduleDefinition(
-    job=ecommerce_job,
+trading_sys_schedule = ScheduleDefinition(
+    job=strategy_calc_job,
     cron_schedule="30 5 * * 1-5",
     default_status=DefaultScheduleStatus.RUNNING,
 )
 
-
 defs = Definitions(
-    assets=[hackernews_top_story_ids, hackernews_top_stories],
-    jobs=[ecommerce_job],
-    schedules=[ecommerce_schedule],
+    assets=[load_tickers, load_ticker_data, combine_data, clean_data, calc_strategy, calc_scores],
+    jobs=[strategy_calc_job],
+    schedules=[trading_sys_schedule]
 )
+
---
+++ scores_calculations.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy.stats import spearmanr
+
+def annualized_sharpe_ratio(returns, risk_free_rate=0, trading_days=252):
+    excess_returns = returns - risk_free_rate
+    sharpe_ratio = np.mean(excess_returns) / np.std(excess_returns, ddof=1)
+    return sharpe_ratio * np.sqrt(trading_days)
+
+def max_drawdown(returns):
+    cumulative = (1 + returns).cumprod()
+    peak = cumulative.cummax()
+    drawdown = (cumulative - peak) / peak
+    return drawdown.min()
+
+def information_coefficient(predicted_returns, actual_returns):
+    return spearmanr(predicted_returns, actual_returns)[0]
+
+def hit_rate(predicted_returns, actual_returns):
+    return np.mean(np.sign(predicted_returns) == np.sign(actual_returns))
+
+def annualized_volatility(returns, trading_days=252):
+    return np.std(returns) * np.sqrt(trading_days)
+
+def annualized_return(returns, trading_days=252):
+    cumulative_return = (1 + returns).prod()
+    return cumulative_return**(trading_days / len(returns)) - 1
+
+def calmar_ratio(returns, trading_days=252):
+    ann_return = annualized_return(returns, trading_days)
+    max_dd = max_drawdown(returns)
+    return ann_return / abs(max_dd) if max_dd != 0 else np.nan
